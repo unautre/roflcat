27d898314273d90e08b363d5964b3586f5a14d29
unautre <dylan.werner.meier@gmail.com>
2022-01-30 13:07:57 +0100

    24bit TrueColor terminal

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 )
 
@@ -20,12 +21,25 @@ const GREEN = 32
 const YELLOW = 33
 const BLUE = 34
 
+func rainbow(freq float64, i int) (int, int, int) {
+	phase := freq * float64(i)
+	var red int = int(math.Sin(phase+0)*127.0 + 128.0)
+	var green int = int(math.Sin(phase+2.0*math.Pi/3.0)*127.0 + 128.0)
+	var blue int = int(math.Sin(phase+4.0*math.Pi/3.0)*127.0 + 128.0)
+	//"#%02X%02X%02X" % [ red, green, blue ]
+	return red, green, blue
+}
+
 func roflize(scanner *bufio.Scanner) {
+	lineCount := 0
 	for scanner.Scan() {
 		line := scanner.Text()
+        lineCount += 1
 
 		for index, runeValue := range line {
-			fmt.Printf("\033[%dm%s", (index%10)+30, string(runeValue))
+			red, green, blue := rainbow(0.1, index + lineCount)
+			fmt.Printf("\033[38;2;%d;%d;%dm%s", red, green, blue, string(runeValue))
+			//fmt.Printf("\\033[38;2;%d;%d;%dm%s\n", red, green, blue, string(runeValue))
 		}
 	}
 }
